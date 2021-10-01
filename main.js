ae6aa5859292aa22be5af45a56d0ccd4d074fe41
MatiasEGonzalez <matiasgonzalez110@gmail.com>
2021-10-01 01:17:24 -0300

    Tareas de coder

---
+++ main.js
@@ -0,0 +1,5 @@
+const ESPACIO = ' ';
+let nombre = prompt('Ingrese su nombre Aqui');
+let apellido = prompt('Ingrese su apellido Aqui');
+let mensajeBienvenida = 'Bienvenido a mi App' + ESPACIO + nombre + ESPACIO + apellido;
+alert(mensajeBienvenida)
